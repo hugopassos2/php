c1bec1b30457e229d52617821eb180129a8c7efc
Hugo <hugo.passos@hotmail.co.uk>
2018-10-11 20:47:59 -0300

    Métodos de lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -57,6 +57,50 @@ class Usuario {
 	}
 
 
+	public static function getList(){//Não usei a palavra $this, então esse método pode ser STATIC. A vantagem de ser STATIC é que eu não preciso instanciar o objeto, basta chamar Usuario::getList()
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+
+	}
+
+
+	public static function search($login){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(':SEARCH'=>"%".$login."%"));
+
+	}
+
+
+	public function login($login, $password){
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(":LOGIN"=>$login,
+													":PASSWORD"=>$password
+													));
+
+		if(count($results) > 0){
+
+			$row = $results[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+		} else {
+
+			throw new Exception("Login e/ou senha inválidos.");
+
+		}
+
+	}
+
+
 	public function __toString(){
 
 		return json_encode(array(
--- index.php
+++ index.php
@@ -8,11 +8,28 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);*/
 
+
+/*//Carrega um usuário
 $user = new Usuario();
 
 $user->loadById(3);
 
-echo $user;
+echo $user;*/
+
+
+/*//Carrega uma lista de usuários
+$lista = Usuario::getList();
+
+echo json_encode($lista);*/
+
+/*//Carrega uma lista de usuários buscando pelo login
+$search = Usuario::search("jo");
+echo json_encode($search);*/
+
+//Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("user", "12345");
+echo $usuario;
 
 
 ?>
